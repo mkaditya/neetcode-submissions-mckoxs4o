49ef2c4ff471cc6cfeabfb96079d1c5171ab118d
mkaditya <37263212+mkaditya@users.noreply.github.com>
2026-04-10 08:29:04 -0400

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return self.dp_tabulation(m,n)
+
+    # dp with memoization top-down
+    def dp_memoization(self, m, n):
+        cache = {}
+        def dfs(x,y):
+            if x == m - 1 and y == n - 1:
+                return 1 # goal reached
+
+            if x >= m or y >= n:
+                return 0
+            
+            if (x,y) not in cache:
+                cache[(x,y)] = dfs(x, y+1) + dfs(x+1, y)
+            
+            return cache[(x,y)]
+        
+        return dfs(0, 0)
+
+    def dp_tabulation(self, m, n):
+        # so that when we do i+1, j and i, j+1 it won't go out of bounds, addidtional row & col always have zero value
+        dp = [[0] * (n+1) for _ in range(m+1)]
+
+        # Base case: 1 way to be at the destination (you're already there)
+        dp[m-1][n-1] = 1
+
+        # now lets walk backwards, both in row & col manner while populating rest of entries.
+        for r_idx in reversed(range(m)):
+            for c_idx in reversed(range(n)):
+                if r_idx == m-1 and c_idx == n-1:
+                    continue # skip destination cell
+                dp[r_idx][c_idx] = dp[r_idx + 1][c_idx] + dp[r_idx][c_idx+1]
+        return dp[0][0]
