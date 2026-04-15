25215d472f7ebc6f610f2a1a2ce8a071ea920e9b
mkaditya <37263212+mkaditya@users.noreply.github.com>
2026-04-15 08:02:17 -0400

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,15 @@
+# TODO: Aditya do this again
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = [(p,s) for p,s in zip(position, speed)]
+        cars.sort(reverse=True) # keep car further down the road first
+
+        fleets = 0
+        last_arrival_time = 0
+
+        for pos, spd in cars:
+            time_to_destination = (target - pos)/spd
+            if time_to_destination > last_arrival_time:
+                fleets += 1
+                last_arrival_time = time_to_destination
+        return fleets
