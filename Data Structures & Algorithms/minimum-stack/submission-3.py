5909a0380ba85452709fc966104470783f02e7d3
mkaditya <37263212+mkaditya@users.noreply.github.com>
2026-04-14 22:23:55 -0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,31 @@
+
+# TODO: Aditya apparently we can do it wit one stack & encoding, take a look
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, val: int) -> None:
+        min_val = val
+        if self.min_stack:
+            min_val = min(val, self.min_stack[-1])
+        self.stack.append(val)
+        self.min_stack.append(min_val)
+
+        
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+            self.min_stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+        
