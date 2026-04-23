0d31aefddfad9f8eeac437227e894e52e31de527
mkaditya <37263212+mkaditya@users.noreply.github.com>
2026-04-23 07:41:28 -0400

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = defaultdict(list) # captures dependent courses
+        indegree = defaultdict(int) # captures how many courses we are dependent on
+
+        # a, b => b --is dependency of --> a
+        for course, prereq in prerequisites:
+            adj_list[course].append(prereq)
+            indegree[prereq] += 1
+
+        q = deque()
+        for course in range(numCourses):
+            if indegree[course] == 0: # i.e no prereq
+                q.append(course)
+        
+
+        completed_courses = 0
+        while q:
+            course = q.popleft()
+            completed_courses += 1
+
+            dependent_courses = adj_list[course]
+            for dep in dependent_courses:
+                indegree[dep] -= 1
+                if indegree[dep] == 0:
+                    q.append(dep)
+        
+        return completed_courses == numCourses
