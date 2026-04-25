7153809806a91ccb60be7cde9790771ba9a38aa7
mkaditya <37263212+mkaditya@users.noreply.github.com>
2026-04-25 09:14:09 -0400

    Add: course-schedule-iv - submission-0

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adj_list = defaultdict(list)
+        indegree = defaultdict(int)
+        prereq_map = defaultdict(set)
+
+        for prereq, course in prerequisites:
+            adj_list[prereq].append(course)
+            indegree[course] += 1
+
+        q = deque()
+        for idx in range(numCourses):
+            if indegree[idx] == 0:
+                q.append(idx)
+
+        
+        while q:
+            course = q.popleft()
+            for dep in adj_list[course]:
+                prereq_map[dep].add(course)
+                prereq_map[dep].update(prereq_map[course])
+                indegree[dep] -= 1
+                if indegree[dep] == 0:
+                    q.append(dep)
+        
+        res = []
+        for prereq, course in queries:
+            res.append(prereq in prereq_map[course])
+        return res
