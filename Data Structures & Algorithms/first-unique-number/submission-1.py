79e3a3999430e742504825bb41991b2404cce92b
mkaditya <37263212+mkaditya@users.noreply.github.com>
2026-05-20 07:55:40 -0400

    Add: first-unique-number - submission-1

---
+++ Data Structures & Algorithms/first-unique-number/submission-1.py
@@ -0,0 +1,26 @@
+class FirstUnique:
+
+    def __init__(self, nums: List[int]):
+        self.q = OrderedDict()
+        self.is_duplicated = {}
+        for num in nums:
+            self.add(num)
+
+    def add(self, value: int) -> None:
+        if value in self.is_duplicated:
+            if not self.is_duplicated[value]:
+                self.q.pop(value)
+                self.is_duplicated[value] = True
+        else:
+            self.q[value] = None
+            self.is_duplicated[value] = False
+
+    def showFirstUnique(self) -> int:
+        return next(iter(self.q)) if self.q else -1
+        
+
+
+# Your FirstUnique object will be instantiated and called as such:
+# obj = FirstUnique(nums)
+# param_1 = obj.showFirstUnique()
+# obj.add(value)
