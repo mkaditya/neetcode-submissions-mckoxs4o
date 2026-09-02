d8c4ddfe6d17615c1d4d2a85e4346df3767aea2e
mkaditya <37263212+mkaditya@users.noreply.github.com>
2026-09-02 08:16:16 -0400

    Add: frequency-of-the-most-frequent-element - submission-0

---
+++ Data Structures & Algorithms/frequency-of-the-most-frequent-element/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        max_freq, curr_window = 0, 0
+        l, r = 0, 0
+        while r < len(nums):
+            curr_window += nums[r]
+            while (r - l + 1) * nums[r] > curr_window + k:
+                curr_window -= nums[l]
+                l += 1
+            max_freq = max(max_freq, r - l + 1)
+            r += 1
+        return max_freq
